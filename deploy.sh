d399d23417140442427198720377db6e301253c8
Sebastian Berlin <berlin.sebastian@gmail.com>
2015-11-30 11:14:29 +0100

    Added file with list of scores when deploying.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 DESTINATION_PATH=~/Dropbox/Public/sambabot
 
+ls -1B scores | egrep -v "scores-list.txt" > scores/scores-list.txt
 for d in $(find . -type d -not -path "." -not -path "*/.*")
 do
     mkdir -v $DESTINATION_PATH/$d
