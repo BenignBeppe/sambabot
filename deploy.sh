08bbb37b13d3a914325a2ae122baa37d176abffd
Sebastian Berlin <berlin.sebastian@gmail.com>
2016-02-06 23:30:41 +0100

    Added -g flag to deploy.sh for only generating lists (and not actually deploying) and med the script a bit more informative. Removed lists from repo, as they should be generated with the script.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,39 @@
 #! /usr/bin/env bash
 
 DESTINATION_PATH=~/Dropbox/Public/sambabot
+SCORE_LIST_PATH=score-list.txt
+NOTE_TYPE_LIST_PATH=note-type-list.txt
+ONLY_GENERATE=0
 
-ls -1B scores | sed -r "s/\.json//g" > score-list.txt
-find sounds -type f > note-type-list.txt
-for d in $(find . -type d -not -path "." -not -path "*/.*")
-do
-    mkdir -v $DESTINATION_PATH/$d
+while getopts ":g" opt; do
+  case $opt in
+    g)
+      ONLY_GENERATE=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
 done
-for f in $(find . -type f -not -path "*/.*" -not -name "*~" -not -name "#*#" )
-do
-    cp -v $f $DESTINATION_PATH/$f
-done
+
+echo === Generating lists ===
+ls -1B scores | sed -r "s/\.json//g" > $SCORE_LIST_PATH
+echo Generated $SCORE_LIST_PATH with $(wc -l $SCORE_LIST_PATH | egrep -o "^[0-9]+") scores.
+find sounds -type f > $NOTE_TYPE_LIST_PATH
+echo Generated $NOTE_TYPE_LIST_PATH with $(wc -l $NOTE_TYPE_LIST_PATH | egrep -o "^[0-9]+") note type sounds.
+
+if [ $ONLY_GENERATE = 0 ]
+then
+    echo === Creating directories ===
+    for d in $(find . -type d -not -path "." -not -path "*/.*")
+    do
+        mkdir -v $DESTINATION_PATH/$d
+    done
+
+    echo === Copying files ===
+    for f in $(find . -type f -not -path "*/.*" -not -name "*~" \
+               -not -name "#*#" -not -name "deploy.sh" )
+    do
+        cp -v $f $DESTINATION_PATH/$f
+    done
+fi
